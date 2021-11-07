d8e30537b0d6f0e166fb0a004f33d6f3580511d1
Dayanne <dayanneayres@gmail.com>
2021-11-07 15:53:00 -0300

    Utilizando metodos de string

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 print(url)
 
-url_base = url[0:27]
+# Separando url base e parâmetros.
+indice_interrogação = url.find("?")
+url_base = url[:indice_interrogação]
 print(url_base)
-
-url_parametros = url[28:]
+url_parametros = url[indice_interrogação+1:]
 print(url_parametros)
+
+# Buscando por parametros especificos.
+parametro_busca = "moedaDestino"
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find("&", indice_valor)
+# Se for o ultimo parametro, nao tem e comercial.
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
